a6c33b939414bc0dd1cfb9cac4315db315e3d098
Howtotails Co., Ltd <43520388+howtotailscompany@users.noreply.github.com>
2023-02-18 14:37:45 +0700

    Insert all to table

---
+++ insertall_into_emp.sql
@@ -0,0 +1,25 @@
+INSERT ALL 
+      into  UT_ADMIN.EMP (ENAME,EMPNO,SAL) values ('KING',7839,5000)
+      into  emp (ENAME,EMPNO,SAL) values ('SCOTT',7788,3000)                 
+      into  emp (ENAME,EMPNO,SAL) values ('FORD',7902,3000) 
+      into  emp (ENAME,EMPNO,SAL) values ('JONES',7566,2975)      
+      into  emp (ENAME,EMPNO,SAL) values ('BLAKE',7698,2850)    
+      into  emp (ENAME,EMPNO,SAL) values ('CLARK',7782,2450)
+      into  emp (ENAME,EMPNO,SAL) values ('ALLEN',7499,1600)    
+      into  emp (ENAME,EMPNO,SAL) values ('TURNER',7844,1500)
+      into  emp (ENAME,EMPNO,SAL) values ('MILLER',7934,1300)
+      into  emp (ENAME,EMPNO,SAL) values ('WARD',7521,1250)    
+      into  emp (ENAME,EMPNO,SAL) values ('MARTIN',7654,1250)
+      into  emp (ENAME,EMPNO,SAL) values ('ADAMS',7876,1100)
+      into  emp (ENAME,EMPNO,SAL) values ('JAMES',7900,950)    
+      into  emp (ENAME,EMPNO,SAL) values ('SMITH',7369,800)
+SELECT * FROM dual;
+     
+                                 
+               
+               
+                 
+               
+                
+                 
+                 
